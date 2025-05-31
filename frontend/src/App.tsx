ce27c2d9a65e329de361127b6b9d59b3870c58a3
sonneko27 <sonneko27@gmail.com>
2025-05-31 14:45:57 +0000

    update clean template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,12 @@
-import './App.scss';
+import { Component } from 'solid-js';
 
-import { Component, createSignal } from 'solid-js';
-import Counter from './Counter';
 
 const App: Component = () => {
-  const [counter, setCounter] = createSignal(0);
-  setInterval(setCounter, 1000, (c: number) => c + 1);
-
   return (
     <>
-      <div>
-        <h1 class="header">{counter()}</h1>
-      </div>
-      <Counter />
+      <h1>Hello World!</h1>
     </>
-  );
+  )
 };
 
 export default App;
